98f2c92e8e70a2c776c7c8a0066082438e3b9331
christezeng <christezeng@gmail.com>
2021-09-28 20:12:13 +0800

    1FK Lab1

---
+++ src/lab0/linux-5.14.3/connect.sh
@@ -0,0 +1,1 @@
+riscv64-unknown-linux-gnu-gdb /bccts/os21fall/src/lab0/linux-5.14.3/vmlinux
---
+++ src/lab0/linux-5.14.3/qemu.sh
@@ -0,0 +1,1 @@
+qemu-system-riscv64 -nographic -machine virt -kernel arch/riscv/boot/Image -device virtio-blk-device,drive=hd0 -append "root=/dev/vda ro console=ttyS0" -bios default -drive file=../rootfs.img,format=raw,id=hd0
