3e7c8403e0f561f2273e226009ded2ce4ddcff54
christezeng <christezeng@gmail.com>
2021-11-09 22:08:57 +0800

    Lab3 Basically Completed

---
+++ src/lab3/.history/arch/riscv/kernel/proc_20211109151418.c
@@ -0,0 +1,28 @@
+//arch/riscv/kernel/proc.c
+#include "proc.h"
+#include "mm.h"
+
+extern void __dummy();
+
+struct task_struct* idle;           // idle process
+struct task_struct* current;        // 指向当前运行线程的 `task_struct`
+struct task_struct* task[NR_TASKS]; // 线程数组，所有的线程都保存在此
+
+void task_init() {
+    // 1. 调用 kalloc() 为 idle 分配一个物理页
+    // 2. 设置 state 为 TASK_RUNNING;
+    // 3. 由于 idle 不参与调度 可以将其 counter / priority 设置为 0
+    // 4. 设置 idle 的 pid 为 0
+    // 5. 将 current 和 task[0] 指向 idle
+
+    /* YOUR CODE HERE */
+
+    // 1. 参考 idle 的设置, 为 task[1] ~ task[NR_TASKS - 1] 进行初始化
+    // 2. 其中每个线程的 state 为 TASK_RUNNING, counter 为 0, priority 使用 rand() 来设置, pid 为该线程在线程数组中的下标。
+    // 3. 为 task[1] ~ task[NR_TASKS - 1] 设置 `thread_struct` 中的 `ra` 和 `sp`,
+    // 4. 其中 `ra` 设置为 __dummy （见 4.3.2）的地址， `sp` 设置为 该线程申请的物理页的高地址
+
+    /* YOUR CODE HERE */
+
+    printk("...proc_init done!\n");
+}
