cc8c3c3eafd6e856a10849ccf32ffabec5a2fa57
christezeng <christezeng@gmail.com>
2022-01-16 11:09:06 +0800

    lab5 completed

--- src/lab5/.history/arch/riscv/kernel/trap_20220112174256.c
+++ src/lab5/.history/arch/riscv/kernel/trap_20220112174256.c
@@ -5,40 +5,39 @@
 #include "syscall.h"
 
 struct pt_regs {
-    uint64 x1;
-    uint64 x2;
-    uint64 x3;
-    uint64 x4;
-    uint64 x5;
-    uint64 x6;
-    uint64 x7;
-    uint64 x8;
-    uint64 x9;
-    uint64 x10;
-    uint64 x11;
-    uint64 x12;
-    uint64 x13;
-    uint64 x14;
-    uint64 x15;
-    uint64 x16;
-    uint64 x17;
-    uint64 x18;
-    uint64 x19;
-    uint64 x20;
-    uint64 x21;
-    uint64 x22;
-    uint64 x23;
-    uint64 x24;
-    uint64 x25;
-    uint64 x26;
-    uint64 x27;
-    uint64 x28;
-    uint64 x29;
-    uint64 x30;
-    uint64 x31;
-    uint64 sepc;
     uint64 sstatus;
-    
+    uint64 sepc;
+    uint64 x31;
+    uint64 x30;
+    uint64 x29;
+    uint64 x28;
+    uint64 x27;
+    uint64 x26;
+    uint64 x25;
+    uint64 x24;
+    uint64 x23;
+    uint64 x22;
+    uint64 x21;
+    uint64 x20;
+    uint64 x19;
+    uint64 x18;
+    uint64 x17;
+    uint64 x16;
+    uint64 x15;
+    uint64 x14;
+    uint64 x13;
+    uint64 x12;
+    uint64 x11;
+    uint64 x10;
+    uint64 x9;
+    uint64 x8;
+    uint64 x7;
+    uint64 x6;
+    uint64 x5;
+    uint64 x4;
+    uint64 x3;
+    uint64 x2;
+    uint64 x1;
 };
 
 void trap_handler(unsigned long scause, unsigned long sepc, struct pt_regs *regs) {
